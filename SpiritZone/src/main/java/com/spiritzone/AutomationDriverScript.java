49aaa796937fe16054dfed337090e35d40c09e9b
Nishant <Nishant@DESKTOP-1SAS5MA>
2020-10-05 10:51:14 +0530

    Zip+Mail 05-10-2020

--- SpiritZone/src/main/java/com/spiritzone/AutomationDriverScript.java
+++ SpiritZone/src/main/java/com/spiritzone/AutomationDriverScript.java
@@ -1,9 +1,14 @@
 package com.spiritzone;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Properties;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
@@ -77,95 +82,110 @@ public class AutomationDriverScript extends FunctionalKeyword {
 
 	
 	@AfterSuite
-	public void afterSuite() throws Exception {
+	public void afterSuite() throws Exception 
+	{
 		
 		String ConfigMailReport = ConfigReader.getInstance().getValue(PropertyConfigs.ConfigEmail);
 		
 		if (ConfigMailReport.equalsIgnoreCase("Yes"))
 		{
 			
-		System.out.println("----------------------------------------------------");
-		System.out.println("----------------------------------------------------");
-		System.out.println("---------Starting Procedure to Send Mail------------");
-		System.out.println("----------------------------------------------------");
-		System.out.println("----------------------------------------------------");
-		
-		String SOURCE_FOLDER = TestLauncher.excutionFolderEXCELFile;
-		//String SOURCE_FOLDER= ConfigReader.getInstance().getValue(PropertyConfigs.SOURCE_FOLDER_TO_ZIP);
-		String OUTPUT_ZIP_FILE=ConfigReader.getInstance().getValue(PropertyConfigs.OUTPUT_ZIP_FILE);
-		
-		ZipUtils appZip = new ZipUtils();
-        appZip.generateFileList(new File(SOURCE_FOLDER));
-        appZip.zipIt(OUTPUT_ZIP_FILE);
-		
-		String MyEmailID = ConfigReader.getInstance().getValue(PropertyConfigs.MyEmailID);
-		String MyName = ConfigReader.getInstance().getValue(PropertyConfigs.MyName);
-		String ToEmail = ConfigReader.getInstance().getValue(PropertyConfigs.ToEmailID);
-		
-		
-		storedPassword = new StorePassWord();
-		Properties mailProp=System.getProperties();
-		String mailHost=ConfigReader.getInstance().getValue(PropertyConfigs.mailHost);
-		int mailPort=Integer.parseInt(ConfigReader.getInstance().getValue(PropertyConfigs.mailPort));
-		mailProp.put("mail.smtp.auth", "true");
-		mailProp.put("mail.smtp.starttls.enable", "true");
-		//mailProp.put("mail.smtp.starttls.enable", "true");
-		mailProp.put("mail.smtp.host", mailHost);
-		mailProp.put("mail.smtp.port", mailPort);
-		// outMail login Details
-		final String outmailUsername = MyEmailID;
-		final String outmailPassword = storedPassword.decrypt("AhYqFi8DFBQy6Jm5gI3upw==");
-		//final String outmailPassword = "Kox44414";
-		
-		Authenticator auth = new Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(outmailUsername, outmailPassword);
-			}
-		};
+					System.out.println("----------------------------------------------------");
+					System.out.println("----------------------------------------------------");
+					System.out.println("---------Starting Procedure to Send Mail------------");
+					System.out.println("----------------------------------------------------");
+					System.out.println("----------------------------------------------------");
+					
+					String SOURCE_FOLDER = TestLauncher.excutionFolderEXCELFile;
+					//String SOURCE_FOLDER= ConfigReader.getInstance().getValue(PropertyConfigs.SOURCE_FOLDER_TO_ZIP);
+					String OUTPUT_ZIP_FILE = System.getProperty("user.dir") + "/ZIP_FILES_TO_MAIL/"+"Custom.zip";
+					//String OUTPUT_ZIP_FILE=ConfigReader.getInstance().getValue(PropertyConfigs.OUTPUT_ZIP_FILE);
+					
+					File file = new File(SOURCE_FOLDER);
+					String zipFileName = OUTPUT_ZIP_FILE;
+					
+					FileOutputStream fos = new FileOutputStream(zipFileName);
+			        ZipOutputStream zos = new ZipOutputStream(fos);
 
-		Session session = Session.getInstance(mailProp, auth);
+			        zos.putNextEntry(new ZipEntry(file.getName()));
+			        
+			        byte[] bytes = Files.readAllBytes(Paths.get(SOURCE_FOLDER));
+			        zos.write(bytes, 0, bytes.length);
+			        zos.closeEntry();
+			        zos.close();
+					
+//					ZipUtils appZip = new ZipUtils();
+//			        appZip.generateFileList(new File(SOURCE_FOLDER));
+//			        appZip.zipIt("");
+					
+					String MyEmailID = ConfigReader.getInstance().getValue(PropertyConfigs.MyEmailID);
+					String MyName = ConfigReader.getInstance().getValue(PropertyConfigs.MyName);
+					String ToEmail = ConfigReader.getInstance().getValue(PropertyConfigs.ToEmailID);
+					
+					
+					storedPassword = new StorePassWord();
+					Properties mailProp=System.getProperties();
+					String mailHost=ConfigReader.getInstance().getValue(PropertyConfigs.mailHost);
+					int mailPort=Integer.parseInt(ConfigReader.getInstance().getValue(PropertyConfigs.mailPort));
+					mailProp.put("mail.smtp.auth", "true");
+					mailProp.put("mail.smtp.starttls.enable", "true");
+					//mailProp.put("mail.smtp.starttls.enable", "true");
+					mailProp.put("mail.smtp.host", mailHost);
+					mailProp.put("mail.smtp.port", mailPort);
+					// outMail login Details
+					final String outmailUsername = MyEmailID;
+					final String outmailPassword = storedPassword.decrypt("AhYqFi8DFBQy6Jm5gI3upw==");
+					//final String outmailPassword = "Kox44414";
+					
+					Authenticator auth = new Authenticator() {
+						protected PasswordAuthentication getPasswordAuthentication() {
+							return new PasswordAuthentication(outmailUsername, outmailPassword);
+						}
+					};
 
-		try
-		{
-			MimeMessage msg = new MimeMessage(session);
+					Session session = Session.getInstance(mailProp, auth);
 
-			msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
-			msg.addHeader("Content-Transfer-Encoding", "8bit");
-			msg.setSentDate(new Date());
-			msg.setFrom(new InternetAddress(MyEmailID,MyEmailID));
-			msg.setReplyTo(InternetAddress.parse(MyName, false));
-			msg.setSubject("Result For Your Execution Is Here", "UTF-8");	
-			msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(ToEmail, false));
-			BodyPart messageBodyPart = new MimeBodyPart();
-			messageBodyPart.setText("Hello, Good day! \n"
-					+ "\n"
-					+ "All scenarios have been executed. Please find the attached report of the execution.  \n"
-					+ "\n"
-					+ "Thanks,\n"
-					+ "Anmol.");
-			Multipart multipart = new MimeMultipart();
+					try
+					{
+						MimeMessage msg = new MimeMessage(session);
 
-			// Set text message part
-			multipart.addBodyPart(messageBodyPart);
-			// Second part is attachment
-			messageBodyPart = new MimeBodyPart();
-			
-			//DataSource sourceZip = new FileDataSource(zip.toString());
-			File file2 = new File(OUTPUT_ZIP_FILE);
-			messageBodyPart = new MimeBodyPart();
-			DataSource source = new FileDataSource(file2.getAbsolutePath());	
-			messageBodyPart.setDataHandler(new DataHandler(source));
-			messageBodyPart.setFileName(folderDate+"OUTPUT");
-			multipart.addBodyPart(messageBodyPart);
-			// Send the complete message parts
-			msg.setContent(multipart);
-			Transport.send(msg);  
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-		//zip.delete();
-	}	
+						msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+						msg.addHeader("Content-Transfer-Encoding", "8bit");
+						msg.setSentDate(new Date());
+						msg.setFrom(new InternetAddress(MyEmailID,MyEmailID));
+						msg.setReplyTo(InternetAddress.parse(MyName, false));
+						msg.setSubject("Result For Your Execution Is Here", "UTF-8");	
+						msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(ToEmail, false));
+						BodyPart messageBodyPart = new MimeBodyPart();
+						messageBodyPart.setText("Hello, Good day! \n"
+								+ "\n"
+								+ "All scenarios have been executed. Please find the attached report of the execution.  \n"
+								+ "\n"
+								+ "Thanks,\n"
+								+ "Anmol.");
+						Multipart multipart = new MimeMultipart();
+
+						// Set text message part
+						multipart.addBodyPart(messageBodyPart);
+						// Second part is attachment
+						messageBodyPart = new MimeBodyPart();
+						
+						//DataSource sourceZip = new FileDataSource(zip.toString());
+						File file2 = new File(zipFileName);
+						messageBodyPart = new MimeBodyPart();
+						DataSource source = new FileDataSource(file2.getAbsolutePath());	
+						messageBodyPart.setDataHandler(new DataHandler(source));
+						messageBodyPart.setFileName(folderDate+".zip");
+						multipart.addBodyPart(messageBodyPart);
+						// Send the complete message parts
+						msg.setContent(multipart);
+						Transport.send(msg);  
+					}
+					catch (Exception e) {
+						e.printStackTrace();
+					}
+					//zip.delete();
+				}	
 		
 	}
 
